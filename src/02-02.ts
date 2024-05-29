055547c0befd27951f244ac8de30c1ef5d330ef6
voductien94798 <voductien2003@gmail.com>
2024-05-29 09:25:22 +0700

    ts02-02 done

---
+++ src/02-02.ts
@@ -0,0 +1,11 @@
+// Explicit Types
+//Khai báo kiểu dữ liệu thì sau dấu 2 chấm
+let count: number = 123;
+let StudentName: String = 'voductien';
+let isActive: boolean = true;
+const arrNumber: number[] = [1, 12, 3];
+
+//Infered types
+// có thể tự hiểu kiểu dữ liệu nhưng có 1 số trường hợp là any
+let StudentName1 = 'voductien';
+let isActive2 = true;
