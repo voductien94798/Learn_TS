bf42ca7c153154af78f72b4095e565f6945e017c
voductien94798 <voductien2003@gmail.com>
2024-05-29 10:03:31 +0700

    ts02-04 done

---
+++ src/ts02-04.ts
@@ -0,0 +1,20 @@
+// 2 == '2';
+// 2 === '3';
+//allways use === in my code oke ?
+
+// const  A = {id: 1, name: 'tien'}
+// const B = A
+
+// console.log(A === B)
+// console.log(A === {id:1 , name: 'tien'}) // false
+
+// const A = {id: 1, name:"tien"}
+// const B = {id: 1, name:"tien"}
+
+// A.id === B.id
+let student = {};
+student = { id: 1, name: 'tien' };
+if (student?.id) {
+}
+// chua khoi tao la undefined
+// khoi tao roi ma khong co du lieu la null
