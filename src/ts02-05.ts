bbe37fae543dcf324d65a8530cfbfa8988bfe526
voductien94798 <voductien2003@gmail.com>
2024-05-29 10:15:14 +0700

    ts02-05 done

---
+++ src/ts02-05.ts
@@ -0,0 +1,12 @@
+//USE CONST
+// hằng số upper case
+const API_URL = '...';
+
+// cho biến không muốn hay đổi camelcase
+const PORT_NUMBER = 8888;
+
+//USE LET
+//KHI CÓ NHU CẦU THAY ĐỔI BẰNG PHÉP GÁN
+
+let numberList = [1, 2, 3];
+numberList = [];
